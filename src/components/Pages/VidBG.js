74ffc5df96d87755e1cbdf49fb6c78bc034265e4
pablocodeglia <codeglia.pablo@gmail.com>
2022-10-09 19:40:49 +0100

    Configure React app for deployment to GitHub Pages

--- src/components/Pages/VidBG.js
+++ src/components/Pages/VidBG.js
@@ -1,14 +1,7 @@
 import sneakers from "../../assets/video/sneakers.mp4";
 
 const VidBG = () => {
-  return (
-    <video
-      src="https://maricastro.com.br/pablotest/sneakers.mp4"
-      autoPlay
-      loop
-      muted
-    />
-  );
+  return <video src={sneakers} autoPlay loop muted />;
 };
 
 export default VidBG;
